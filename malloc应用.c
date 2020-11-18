832c359589ae1528dd7720ea921bbe4dd8906b75
migicLin <74419601+migicLin@users.noreply.github.com>
2020-11-18 15:25:36 +0800

    Update malloc应用.c
    字符的输入，while和for两种方法

--- malloc应用.c
+++ malloc应用.c
@@ -16,3 +16,23 @@ int main(void)
 	}
 	return 0;
 }
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int n;
+	int* a;
+	int i = 0;
+	scanf("%d", &n);
+	a = (int*)malloc(n * sizeof(int));
+	while ((i < n) && (scanf("%d", (a + i)) == 1))//如果输入是整数，==1；能运行，否则等于0；跳出
+	{
+		i++;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
+//暂且可用当作一个变量，下面的更安全
