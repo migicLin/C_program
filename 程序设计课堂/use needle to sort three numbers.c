798cc12c77e16ed46956922f98da193c86cd2bf2
migicLin <74419601+migicLin@users.noreply.github.com>
2020-11-25 14:34:08 +0800

    Update use needle to sort three numbers.c

--- 程序设计课堂/use needle to sort three numbers.c
+++ 程序设计课堂/use needle to sort three numbers.c
@@ -26,3 +26,40 @@ int main(void)
 	printf("%d %d %d", a, b, c);
 	return 0;
 }
+
+#include<stdio.h>
+void change(int* a, int* b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main(void)
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int max = a;
+	if (max < b)
+	{
+		change(&max, &b);
+	}
+	if (max < c)
+	{
+		change(&max, &c);
+	}
+	int min = a;
+	if (min > b)
+	{
+		change(&min, &b);
+	}
+	if (min > c)
+	{
+		change(&min, &c);
+	}
+	b = a + b + c - max - min;
+	a = min;
+	c = max;
+	printf("%d %d %d", a, b, c);
+	return 0;
+}
