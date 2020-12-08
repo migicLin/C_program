3fcb7db16a9089828c5f52584efc544cbdabfe82
migicLin <74419601+migicLin@users.noreply.github.com>
2020-12-08 15:43:49 +0800

    Create two hand numbers make left numbers become zero.c

---
+++ 程序设计课堂/two hand numbers make left numbers become zero.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int a[100][100];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i >= j)
+			{
+				a[i][j] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
