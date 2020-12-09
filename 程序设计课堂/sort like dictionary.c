5b701e30080bace727b90bf0290a16ec87875e38
migicLin <74419601+migicLin@users.noreply.github.com>
2020-12-09 16:23:05 +0800

    Create sort like dictionary.c
    数组比较大小，用循环的方法。
    将字符串交换位置，可以用strcpy将后面的赋值给前面的；头文件是string。

---
+++ 程序设计课堂/sort like dictionary.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char s[5][6];
+	char t[6];
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%s", s[i]);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			for (int l = 0; l < 6; l++)
+			{
+				if (s[i][l] > s[j][l])
+				{
+					break;
+				}
+				if (s[i][l] < s[j][l])
+				{
+					strcpy(t, s[i]);
+					strcpy(s[i], s[j]);
+					strcpy(s[j], t);
+					break;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%s\n", s[i]);
+	}
+	return 0;
+}
